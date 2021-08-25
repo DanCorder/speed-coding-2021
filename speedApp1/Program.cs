0876af001c3da3f5842820003b478dff816c95ec
Dan Corder <dev@dancorder.com>
2021-08-25 18:55:57 +0100

    feat: basic solution

---
+++ speedApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace speedApp1
+{
+    class Program
+    {
+        private const string FilePath = @"d:\work\speed-coding-2021\input\"; 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            var lines = System.IO.File.ReadLines(FilePath);
+        }
+    }
+}
