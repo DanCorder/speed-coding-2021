22970a84ae9863c892a61274305c18bc2fe5abc0
Dan Corder <dev@dancorder.com>
2021-08-25 21:55:44 +0100

    feat: part 3

--- speedApp1/Part3.cs
+++ speedApp1/Part3.cs
@@ -1,19 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace speedApp1
 {
     class Part3
     {
-        private const string FilePath = @"d:\work\speed-coding-2021\input\test.txt";
+        private const string WordsPath = @"d:\work\speed-coding-2021\input\part3\code-number-words.txt";
+        private const string DictionaryPath = @"d:\work\speed-coding-2021\input\part3\dictionary.txt";
 
         public static void Run()
         {
-            var lines = System.IO.File.ReadLines(FilePath);
-            var allText = System.IO.File.ReadAllText(FilePath);
+            var codedWords = System.IO.File.ReadLines(WordsPath).Select(w => w.Split(' ').Select(n => Int32.Parse(n)).ToList()).ToList().OrderByDescending(w => w.Count).ToList();
+            var dictionaryByLength = System.IO.File.ReadLines(DictionaryPath).GroupBy(w => w.Length).ToDictionary(w => w.Key, w => w.ToList());
 
-            
-            Console.WriteLine("qq");
+            var mapping = new Dictionary<int, char>
+            {
+                {4, 'd'},
+                {10, 'g'}
+            };
+
+            var count = mapping.Count;
+
+            while (mapping.Count < 23)
+            {
+                var possibleMatchesByWord =
+                    codedWords.ToDictionary(cw => cw, cw => FindMatches(cw, dictionaryByLength[cw.Count], mapping));
+
+                var wordsWithSingleOptions = possibleMatchesByWord.Where(kvp => kvp.Value.Count == 1).ToList();
+                if (wordsWithSingleOptions.Count > 0)
+                {
+                    mapping = wordsWithSingleOptions.First().Value.Single().Value;
+                    codedWords.Remove(wordsWithSingleOptions.First().Key);
+                }
+            }
+
+            Console.WriteLine(String.Join('\n', mapping.OrderBy(kvp => kvp.Key).Select(kvp => $"{kvp.Key}={kvp.Value}")));
+        }
+
+        private static Dictionary<string, Dictionary<int, char>> FindMatches(List<int> codedWord, List<string> dictionaryPossibilities, Dictionary<int, char> mapping)
+        {
+            var ret = new Dictionary<string, Dictionary<int, char>>();
+            foreach (var option in dictionaryPossibilities)
+            {
+                var newMapping = mapping.ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+                var isMatch = true;
+                for (int i = 0; i < codedWord.Count; i++)
+                {
+                    if (newMapping.ContainsKey(codedWord[i]))
+                    {
+                        var ch = newMapping[codedWord[i]];
+                        if (option[i] != ch)
+                        {
+                            isMatch = false;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        newMapping[codedWord[i]] = option[i];
+                    }
+                }
+
+                if (isMatch)
+                {
+                    ret[option] = newMapping;
+                }
+            }
+
+            return ret;
         }
     }
 }
